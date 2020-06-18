742971d383e7f177782e8195056b54c174215d09
victor <victor.no1@hotmail.com>
2020-06-17 20:25:21 -0400

    change jenkinsfile

--- 01Build/src/org/devops/build.groovy
+++ 01Build/src/org/devops/build.groovy
@@ -1,2 +1,12 @@
-
+package org.devops
 //build type
+
+def Build(buildType,buildShell){
+    
+    def buildTools = ["mvn":"m2","ant":"ant","gradle":"GRADLE","npm":"node"]
+    
+    println("current buildtype is ${buildType}")
+    buildHome = tool buildTools[buildType]
+
+    sh"${buildHome}/bin/${buildType} ${buildSHell}"
+}
