625c9524b70c7e40f9a355c240da890835a7b80a
Victor Xu <vxuavalon@gmail.com>
2020-06-28 21:09:30 -0400

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -6,26 +6,11 @@ def Sonarscan(projectName,projectDesc，projectPath){
    //def servers = ["test":"sonarqube-test","prod":"sonarqube-prod"]
    
    //使用sonar服务器
-   //withSonarQubeEnv("sonarqube-test"){
+   withSonarQubeEnv("sonarqube-test"){
    def scannerHome="/usr/local/sonar-scanner-4.3.0.2102-linux"
    //def sonarServer = "http://192.168.1.109:19000"
    def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
    sonarDate = sonarDate - "\n"
-   sh """
-    #${scannerHome}/bin/sonar-scanner -Dsonar.host.url=${sonarServer} \
-    ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
-    -Dsonar.projectName=${projectName} \
-    -Dsonar.projectVersion=${sonarDate} \
-    #-Dsonar.login=admin \
-    #-Dsonar.password=admin \
-    -Dsonar.ws.timeout=30 \
-    -Dsonar.projectDescription=${projectDesc} \
-    -Dsonar.links.homepage=http://www.baidu.com \
-    -Dsonar.sources=${projectPath} \
-    -Dsonar.sourceEncoding=UTF-8 \
-    -Dsonar.java.binaries=target/classes \
-    -Dsonar.java.test.binaries=target/test-classes \
-    -Dsonar.java.surefire.report=target/surefire-reports
-   """
+   sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage="http://www.baidu.com" -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports"
    }
 }
