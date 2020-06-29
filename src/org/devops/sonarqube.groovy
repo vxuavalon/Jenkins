da87c650b41a3ea60f77396c2d98abc55c1dfebc
Victor Xu <vxuavalon@gmail.com>
2020-06-28 21:14:13 -0400

    Update sonarqube.groovy

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -7,10 +7,16 @@ def Sonarscan(projectName,projectDesc，projectPath){
    
    //使用sonar服务器
    withSonarQubeEnv("sonarqube-test"){
-   def scannerHome="/usr/local/sonar-scanner-4.3.0.2102-linux"
-   //def sonarServer = "http://192.168.1.109:19000"
-   def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
-   sonarDate = sonarDate - "\n"
-   sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage="http://www.baidu.com" -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports"
+           def scannerHome="/usr/local/sonar-scanner-4.3.0.2102-linux"
+           //def sonarServer = "http://192.168.1.109:19000"
+           def sonarDate = sh  returnStdout: true, script: 'date  +%Y%m%d%H%M%S'
+           sonarDate = sonarDate - "\n"
+           sh """ 
+                    ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+                    -Dsonar.projectName=${projectName} -Dsonar.projectVersion=${sonarDate} -Dsonar.ws.timeout=30 \
+                    -Dsonar.projectDescription=${projectDesc} -Dsonar.links.homepage=http://www.baidu.com \
+                    -Dsonar.sources=${projectPath} -Dsonar.sourceEncoding=UTF-8 -Dsonar.java.binaries=target/classes \
+                    -Dsonar.java.test.binaries=target/test-classes -Dsonar.java.surefire.report=target/surefire-reports
+              """
    }
 }
